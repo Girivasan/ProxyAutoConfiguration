ffdf65d291a0dea379b3ef85a717cc8ba9e17c6b
Girivasan S K <Girivasan@users.noreply.github.com>
2023-04-12 19:04:47 +0530

    Added files for pac in windows with asp.net 6

---
+++ ProxyAutoConfiguration/Controllers/PacController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProxyAutoConfiguration.Controllers
+{
+  [Route("simple.pac")]
+  [ApiController]
+  public class PacController : ControllerBase
+  {
+    // GET: api/pac
+    [HttpGet]
+    public FileContentResult Get()
+    {
+      var pacFile = System.IO.File.ReadAllBytes("Resources/simple.pac");
+      return new FileContentResult(pacFile, "application/x-ns-proxy-autoconfig");
+    }
+  }
+}
---
+++ ProxyAutoConfiguration/Resources/simple.pac
@@ -0,0 +1,4 @@
+function FindProxyForURL(url, host) {
+    // Proxyman
+    return "PROXY 192.168.29.228:9090";
+}
